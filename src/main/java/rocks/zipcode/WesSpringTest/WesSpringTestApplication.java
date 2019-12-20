bdf46977c1b369e9513b2e93ea82b422e12339bc
wes <wes.jones1597@gmail.com>
2019-12-20 15:15:51 -0500

    start spring io

---
+++ src/main/java/rocks/zipcode/WesSpringTest/WesSpringTestApplication.java
@@ -0,0 +1,13 @@
+package rocks.zipcode.WesSpringTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WesSpringTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WesSpringTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rocks/zipcode/WesSpringTest/WesSpringTestApplicationTests.java
@@ -0,0 +1,13 @@
+package rocks.zipcode.WesSpringTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WesSpringTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
